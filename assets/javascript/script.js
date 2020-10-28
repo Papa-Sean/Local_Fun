13e5c3981b518775c68abe4630d496cd1b4f2b4e
Papa-Sean <smsugrue@gmail.com>
2020-10-27 20:29:59 -0400

    Added COVID API - Gave COVID a home in HTML

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,3 +18,28 @@ function show(data) {
     "<h5><strong>Humidity</strong>:<br> " + data.list[0].main.humidity + "%</h5>" +
     "<h5><strong>Wind Speed</strong>:<br> " + data.list[0].wind.speed + "mph</h5>" ;
 };
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-193.p.rapidapi.com/statistics",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-193.p.rapidapi.com",
+		"x-rapidapi-key": "e3a14c6729mshda36173e5fbd4b6p1cf32fjsn3baecbc37181"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+    var widget = show(response);
+    $("#covid-holder").html(widget);
+});
+function show(response){
+    return "<h3><strong>COVID STATS FOR</strong>:<br> " + response.response[3].country +  "</h3>"+
+    "<h4><strong>STATS FOR</strong>:<br> " + response.response[3].day + "</h4><br>" +
+    "<h4><strong>New Cases</strong>:<br> " + response.response[3].cases.new + "</h4><br>" +
+    "<h4><strong>New Deaths</strong>:<br> " +response.response[3].deaths.new + "</h4>"
+    ;
+    
+
+
+};
